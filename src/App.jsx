eb7759676026b37213106366bff8481aea26d35f
oreste-abizera <oresteabizera11@gmail.com>
2022-05-09 11:12:37 +0200

    feat: qrcode setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,45 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import QRCode from "qrcode.react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        flexWrap: "wrap",
+      }}
+    >
+      <div>
+        <QRCode
+          value="https://twitter.com/AbizeraOreste"
+          style={{ marginRight: 50 }}
+        />
+        <p>Twitter</p>
+      </div>
+      <div>
+        <QRCode
+          value="https://www.linkedin.com/in/oreste-abizera-151bb9194/"
+          style={{ marginRight: 50 }}
+        />
+        <p>LinkedIn</p>
+      </div>
+      <div>
+        <QRCode
+          value="https://github.com/oreste-abizera"
+          style={{ marginRight: 50 }}
+        />
+        <p>github</p>
+      </div>
+      <div>
+        <QRCode
+          value="https://www.instagram.com/o.abizera"
+          style={{ marginRight: 50 }}
+        />
+        <p>instagram</p>
+      </div>
     </div>
-  )
+  );
 }
-
-export default App
